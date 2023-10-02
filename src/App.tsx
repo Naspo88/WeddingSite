3c2ed442ca52fca0ef3394beeab54272ff0b43ac
Roberto Minghi <robertominghi@msn.com>
2023-10-02 13:33:33 +0100

    test some changes 2

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Desi and Roberto Wedding!!</p>
-        <p>Test some changes :P</p>
+        <p>Test some changes 2</p>
       </header>
     </div>
   );
