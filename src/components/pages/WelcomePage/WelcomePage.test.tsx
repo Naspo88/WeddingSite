3946a715b32217e17c2e6b91c8b71e72a84c8b4f
Roberto Minghi <robertominghi@msn.com>
2023-10-30 12:10:29 +0000

    Set up serverless lambda

--- src/components/pages/WelcomePage/WelcomePage.test.tsx
+++ src/components/pages/WelcomePage/WelcomePage.test.tsx
@@ -3,6 +3,8 @@ import WelcomePage from "./WelcomePage";
 
 test("renders learn react link", () => {
   render(<WelcomePage />);
-  const linkElement = screen.getByText("Desi and Roberto Wedding!!");
+  const linkElement = screen.getByText(
+    "Per me si va ne la città dolente, per me si va ne l'etterno dolore, per mesi va tra la perduta gente!"
+  );
   expect(linkElement).toBeInTheDocument();
 });
